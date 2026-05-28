2ba4a5046fec33325654eb6d1140ba86f97a1c57
tom <iza012209@gmail.com>
2026-05-28 14:25:33 +0700

    refactor: Deploy backend và frontend riêng lên vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,16 @@ const nextConfig = {
     unoptimized: true,
   },
   allowedDevOrigins: ['192.168.1.124', '192.168.1.124:3000', 'localhost:3000'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` 
+          : 'http://localhost:8000/api/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
